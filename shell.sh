c0a7dd875290162a7e6b88d17dcd34fd4004939c
dineshadmin <deenadineshdr14@gmail.com>
2022-06-27 15:59:52 +0530

    testing

---
+++ shell.sh
@@ -0,0 +1,16 @@
+
+if [ "$#" -ne "1" ]
+then
+        echo "Usage: sh $0  <dir>"
+        exit 1
+fi
+
+if [ -d "$1" ]
+then
+        echo "Directory exists"
+else
+        mkdir "$1"
+        echo "Directory created"
+fi
+exit 0
+
